6858be841e5d3ab59443caa23229d4bfb4921c64
Felix Imobersteg <felix.imobersteg@gmail.com>
2015-07-13 21:47:02 +0200

    feat(vagrant): added vagrant vm

---
+++ Vagrantfile
@@ -0,0 +1,34 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  #Set box
+  config.vm.box = "ubuntu/trusty64"
+
+  #Do not check fo updates
+  config.vm.box_check_update = false
+
+  #Add private network
+  config.vm.network "private_network", ip: "192.168.192.168"
+
+  #Sync folder using NFS if not windows
+  config.vm.synced_folder ".", "/vagrant",
+    :nfs => !Vagrant::Util::Platform.windows?
+
+  #Virtal Box settings
+  config.vm.provider "virtualbox" do |vb|
+    # Don't boot with headless mode
+    #vb.gui = true
+
+    # Set VM settings
+    vb.customize ["modifyvm", :id, "--memory", "1024"]
+    vb.customize ["modifyvm", :id, "--cpus", 2]
+  end
+
+  #Install dependencies
+  config.vm.provision "shell",
+      inline: "sudo sh /vagrant/vm-init/install.sh"
+end
---
+++ vm-init/install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+##################################################################
+#                                                                #
+# @Version:				0.1                                      #
+# @Author:				Felix Imobersteg  			  			 #
+# Description:                                                   #
+# This script install required VM dependecies                    #
+#													             #
+##################################################################
+
+#Change directory
+cd /tmp
+
+#Update sytem
+apt-get update -y
+apt-get upgrade -y
+
+#Install base packages
+apt-get install make nano vim less zip unzip telnet git curl mercurial ca-certificates -y
+
+#Install go
+curl -s https://storage.googleapis.com/golang/go1.3.linux-amd64.tar.gz | tar -v -C /usr/local -xz
+
+#Install Frontend deps
+apt-get install -y npm nodejs
+ln -s /usr/bin/nodejs /usr/bin/node
+npm install -g grunt-cli bower
+
+#Install docker
+curl -sSL https://get.docker.com/ubuntu/ | sh
+
+#Enable aufs
+apt-get -y install linux-image-extra-$(uname -r) aufs-tools
+apt-get -y install lxc-docker
+service docker restart
+
+#Use docker with vagrant user without sudo
+gpasswd -a vagrant docker
+service docker restart
+
+#Create sample SSL certificate
+mkdir -p /etc/ddesktop
+openssl req -new -newkey rsa:4096 -days 3652 -nodes -x509 -subj "/C=CH/ST=ddesktop/L=ddesktop/O=ddesktop/CN=ddesktop.io" -keyout /etc/ddesktop/key.pem  -out /etc/ddesktop/cert.pem 
+
+#Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#Add go to path
+echo 'export PATH=$PATH:/usr/local/go/bin:/go/bin' >> /home/vagrant/.bashrc
+
+#Cleanup
+apt-get autoremove -y
+apt-get autoclean -y
+
