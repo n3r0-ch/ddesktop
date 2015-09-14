05c29d769467400e690612731770bb2a18477ece
Felix Imobersteg <felix@whatwedo.ch>
2015-09-14 17:46:21 +0200

    qucikfix

--- src/ddesktop/dockerhandler/dockerhandler.go
+++ src/ddesktop/dockerhandler/dockerhandler.go
@@ -1,11 +1,12 @@
 package dockerhandler
 
 import (
-	"github.com/samalba/dockerclient"
-	"github.com/spf13/viper"
 	"log"
 	"strings"
+
 	gouuid "github.com/nu7hatch/gouuid"
+	"github.com/samalba/dockerclient"
+	"github.com/spf13/viper"
 )
 
 func GetClient() dockerclient.DockerClient {
@@ -23,24 +24,24 @@ func PullImage() {
 	log.Println("Finished dowloading image: " + viper.GetString("container.image") + ".")
 }
 
-func CleanUp(){
+func CleanUp() {
 	client := GetClient()
 	log.Println("Remove all existing containers...")
 
 	containers, err := client.ListContainers(true, false, "")
 	if err != nil {
-        log.Fatalln(err)
-    }
-    for _, c := range containers {
-    	if len(c.Names) > 0 {
-    		if strings.HasPrefix(c.Names[0], "/" + viper.GetString("container.prefix")) {
-	    		DeleteContainer(c.Id)
-	    	}
-    	}
-    }
+		log.Fatalln(err)
+	}
+	for _, c := range containers {
+		if len(c.Names) > 0 {
+			if strings.HasPrefix(c.Names[0], "/"+viper.GetString("container.prefix")) {
+				DeleteContainer(c.Id)
+			}
+		}
+	}
 }
 
-func StartContainer() string{
+func StartContainer() string {
 	client := GetClient()
 
 	//Get uuid
@@ -53,45 +54,45 @@ func StartContainer() string{
 	name := viper.GetString("container.prefix") + uuid.String()
 
 	//Get limits
-	cpushares := int64(viper.GetInt("container.cpushares"))
-	memory := int64(viper.GetInt("container.memory") * 1024 * 1024)
+	//	cpushares := int64(viper.GetInt("container.cpushares"))
+	//	memory := int64(viper.GetInt("container.memory") * 1024 * 1024)
 
 	//Create container
 	containerConfig := &dockerclient.ContainerConfig{
-        Image: viper.GetString("container.image"),
-        Memory: memory,
-		CpuShares: cpushares,
-    }
-    containerId, err := client.CreateContainer(containerConfig, name)
-    if err != nil {
-        log.Println(err)
-    }
+		Image: viper.GetString("container.image"),
+		//Memory: memory,
+		//		CpuShares: cpushares,
+	}
+	containerId, err := client.CreateContainer(containerConfig, name)
+	if err != nil {
+		log.Println(err)
+	}
 
-    //Start container
-    hostConfig := &dockerclient.HostConfig{
-		Memory: memory,
-		CpuShares: cpushares,
-    }
-    err = client.StartContainer(containerId, hostConfig)
-    if err != nil {
-        log.Fatal(err)
-    }
+	//Start container
+	hostConfig := &dockerclient.HostConfig{
+	//		Memory: memory,
+	//		CpuShares: cpushares,
+	}
+	err = client.StartContainer(containerId, hostConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    log.Println("Started container with id " + containerId)
+	log.Println("Started container with id " + containerId)
 
-    return containerId
+	return containerId
 }
 
-func GetIP(containerId string) string{
+func GetIP(containerId string) string {
 	client := GetClient()
 	info, err := client.InspectContainer(containerId)
 	if err != nil {
-        log.Println(err)
-    }
+		log.Println(err)
+	}
 	return info.NetworkSettings.IPAddress
 }
 
-func DeleteContainer(containerId string){
+func DeleteContainer(containerId string) {
 	log.Println("Removing container " + containerId)
 	client := GetClient()
 	client.KillContainer(containerId, "SIGKILL")
